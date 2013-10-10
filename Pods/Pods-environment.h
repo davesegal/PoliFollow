00bea7a030bc615f3413acb9d9f47a121b9decda
David Segal <dsegal@gmail.com>
2013-10-10 17:59:55 -0400

    first commit

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,153 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// AFNetworking/NSURLConnection
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 0
+
+// AFNetworking/NSURLSession
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 0
+
+// AFNetworking/Reachability
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 0
+
+// AFNetworking/Security
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 0
+
+// AFNetworking/Serialization
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 0
+
+// AFNetworking/UIKit
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 0
+
+// JSONKit
+#define COCOAPODS_POD_AVAILABLE_JSONKit
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 1.5pre.
+
+// Nimbus
+#define COCOAPODS_POD_AVAILABLE_Nimbus
+#define COCOAPODS_VERSION_MAJOR_Nimbus 1
+#define COCOAPODS_VERSION_MINOR_Nimbus 0
+#define COCOAPODS_VERSION_PATCH_Nimbus 0
+
+// Nimbus/AttributedLabel
+#define COCOAPODS_POD_AVAILABLE_Nimbus_AttributedLabel
+#define COCOAPODS_VERSION_MAJOR_Nimbus_AttributedLabel 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_AttributedLabel 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_AttributedLabel 0
+
+// Nimbus/Badge
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Badge
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Badge 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Badge 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Badge 0
+
+// Nimbus/CSS
+#define COCOAPODS_POD_AVAILABLE_Nimbus_CSS
+#define COCOAPODS_VERSION_MAJOR_Nimbus_CSS 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_CSS 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_CSS 0
+
+// Nimbus/Core
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Core
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Core 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Core 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Core 0
+
+// Nimbus/Interapp
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Interapp
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Interapp 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Interapp 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Interapp 0
+
+// Nimbus/Launcher
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Launcher
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Launcher 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Launcher 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Launcher 0
+
+// Nimbus/Models
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Models
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Models 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Models 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Models 0
+
+// Nimbus/NetworkControllers
+#define COCOAPODS_POD_AVAILABLE_Nimbus_NetworkControllers
+#define COCOAPODS_VERSION_MAJOR_Nimbus_NetworkControllers 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_NetworkControllers 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_NetworkControllers 0
+
+// Nimbus/NetworkImage
+#define COCOAPODS_POD_AVAILABLE_Nimbus_NetworkImage
+#define COCOAPODS_VERSION_MAJOR_Nimbus_NetworkImage 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_NetworkImage 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_NetworkImage 0
+
+// Nimbus/Operations
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Operations
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Operations 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Operations 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Operations 0
+
+// Nimbus/Operations/JSON
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Operations_JSON
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Operations_JSON 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Operations_JSON 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Operations_JSON 0
+
+// Nimbus/Overview
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Overview
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Overview 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Overview 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Overview 0
+
+// Nimbus/PagingScrollView
+#define COCOAPODS_POD_AVAILABLE_Nimbus_PagingScrollView
+#define COCOAPODS_VERSION_MAJOR_Nimbus_PagingScrollView 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_PagingScrollView 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_PagingScrollView 0
+
+// Nimbus/Photos
+#define COCOAPODS_POD_AVAILABLE_Nimbus_Photos
+#define COCOAPODS_VERSION_MAJOR_Nimbus_Photos 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_Photos 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_Photos 0
+
+// Nimbus/WebController
+#define COCOAPODS_POD_AVAILABLE_Nimbus_WebController
+#define COCOAPODS_VERSION_MAJOR_Nimbus_WebController 1
+#define COCOAPODS_VERSION_MINOR_Nimbus_WebController 0
+#define COCOAPODS_VERSION_PATCH_Nimbus_WebController 0
+

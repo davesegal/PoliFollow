e939423937d395d6e71a4c3d4904f1cc5d7699da
David Segal <dsegal@gmail.com>
2013-10-11 15:32:00 -0400

    removed pods and added launch image files

--- Podfile
+++ Podfile
@@ -1,3 +1,2 @@
-platform :ios, '7.0'
-pod 'AFNetworking', '~> 2.0'
-pod 'Nimbus', '~> 1.0.0'
+platform :ios, '6.0'
+pod 'AFNetworking', '~> 2.0.0'
--- Pods/Pods-resources.sh
+++ Pods/Pods-resources.sh
@@ -39,9 +39,6 @@ install_resource()
       ;;
   esac
 }
-install_resource "Nimbus/src/overview/resources/NimbusOverviewer.bundle"
-install_resource "Nimbus/src/photos/resources/NimbusPhotos.bundle"
-install_resource "Nimbus/src/webcontroller/resources/NimbusWebController.bundle"
 
 rsync -avr --copy-links --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
 if [[ "${ACTION}" == "install" ]]; then
